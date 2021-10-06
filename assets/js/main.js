3555f8b1d2408582748c1f5469b47e1a9b04248e
Ben Camarillo <jtby97@gmail.com>
2021-10-05 19:28:21 -0500

    cut down on usless code and completed ingredients form element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,39 @@
-fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=%3CREQUIRED%3E&lon=%3CREQUIRED%3E", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
-		"x-rapidapi-key": "71f6b4f193msh561e6e4b6552ca5p13ba72jsne8db3c8baa6c"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
-
-
-
-
-// let url = 'trailapi-trailapi.p.rapidapi.com'
-
-
-// fetch(url).then(function (res) { 
-//     console.log(res)
+// fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=%3CREQUIRED%3E&lon=%3CREQUIRED%3E", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
+// 		"x-rapidapi-key": "71f6b4f193msh561e6e4b6552ca5p13ba72jsne8db3c8baa6c"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
 // });
+var ilist = {
+	items: [],
+	dlist: null,
+	iadd: null,
+	inputItem: null,
+	init: function() {
+	  ilist.dlist = document.getElementById("drink-list");
+	  ilist.iadd = document.getElementById("ingredient-add"); 
+	  ilist.inputItem = document.getElementById("drink-item");
+	  ilist.iadd.addEventListener("submit", function(e){
+		  e.preventDefault();
+		  var listitem = document.createElement("li");
+		  var listitemtext = document.createTextNode(ilist.inputItem.value);
+			listitem.appendChild(listitemtext);
+		  document.getElementById('ilist.add').appendChild(listitem);
+		  console.log(ilist.iadd);
+	  })// where method is, define function method, is add now will become push
+	  
+	  if (localStorage.items == undefined) {localStorage.items = "[]"; }
+	  ilist.items = JSON.parse(localStorage.items);
+
+	//   ilist.draw();
+
+	}
+  }
+  window.addEventListener("DOMContentLoaded", ilist.init);
