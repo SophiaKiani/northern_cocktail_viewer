dc468251cb6f080ca351ab24c093937494a5c3f6
Sophia Kiani <sophkiani@gmail.com>
2021-10-06 21:38:50 -0500

    updated the overall styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,67 +1,13 @@
-///////////////////////////////////////////////Sophia////////////////////////////////////////
-const  welcomeContainer = document.querySelector(".welcome");
-const  startButtonEl = document.querySelector(".start_button");
+const quizTabBtn = $("#quiz-tab");
+const formTabBtn = $("#form_tab");
+const quizContainer = $("#quiz");
+const formContainer = $("#form");
+const quizWelcomeSection = $(".welcome_section");
+const quizQuestionSection = $(".quiz_section")
+const startBtnEl = $(".start_btn");
+const restartBtnEl = $(".restart_btn");
+const nextBtnEl = $(".next-button-container")
 
-// Info Container
-const  infoContainer = document.querySelector("#info_container");
-// Info Selector
-const timerEl = document.querySelector(".timer");
-const scoreboardEl = document.querySelector(".score");
-const counterEl = document.querySelector(".counter");
-
-// Quiz Container
-const  quizContainer = document.querySelector("#quiz_container");
-// Quiz Selector
-const questionEl = document.querySelector(".question");
-const answerContainer = document.querySelector(".answer_container");
-const answerEl = Array.from(document.getElementsByClassName("answer_button"));
-
-
-// Scoreboard Container
-const scoreboardContainer = document.querySelector("#scoreboard_container");
-const restartButtonEl = document.querySelector(".restart_button");
-
-
-function startQuiz () {
-  startButtonEl.classList.add('hidden');
-  welcomeContainer.classList.add('hidden');
-  infoContainer.classList.remove('hidden');
-  quizContainer.classList.remove('hidden');
-  scoreboardContainer.classList.add('hidden');
-  generateQuestions ();
-  startTimer ();
-  startScoreBoard (); 
-}
-
-
-
-
-
-///////////////////////////////////////////////Bentura////////////////////////////////////////
-// Selectors 
-var timerArea = document.getElementById("timer-area");
-  
-  
-// Global variables 
-var timerInterval;
-var secondsRemaining = 60;
-  
-//timer for quiz 
-function startTimer(){
-  timerInterval = setInterval( function(){
-    timerArea.textContent = secondsRemaining + " seconds left";
-    secondsRemaining--;
-  
-    if( secondsRemaining <0 ){
-        clearInterval(timerInterval);
-        checkForWonGame();
-      }
-    }, 1000)
-  }
-  startTimer();
-   
-   
-  
 const questions = [
   {
     question: "What is the name of a coffee drink containing whiskey?",
@@ -184,179 +130,115 @@ const questions = [
     correctOption: "optionC"
   },
 ]
-  
-  let shuffledQuestions = [] //empty array to hold shuffled selected questions out of all available questions
-  
-  function handleQuestions() { 
-      //function to shuffle and push 10 questions to shuffledQuestions array
-  //app would be dealing with 10questions per session
-      while (shuffledQuestions.length <= 4) {
-          const random = questions[Math.floor(Math.random() * questions.length)]
-          if (!shuffledQuestions.includes(random)) {
-              shuffledQuestions.push(random)
-          }
+
+let shuffledQuestions = []
+let questionNumber = 1 
+let indexNumber = 0 
+
+function tabSelectionQuiz () {
+  quizContainer.removeClass('hidden');
+  quizTabBtn.toggleClass('active');
+  formTabBtn.toggleClass('active');
+  formContainer.addClass('hidden');
+}
+
+function tabSelectionForm () {
+  formContainer.removeClass('hidden');
+  formTabBtn.toggleClass('active');
+  quizTabBtn.toggleClass('active');
+  quizContainer.addClass('hidden');
+}
+
+function startQuiz () {
+  quizWelcomeSection.addClass('hidden');
+  quizQuestionSection.removeClass('hidden');
+  NextQuestion ();
+}
+
+function handleQuestions() { 
+  while (shuffledQuestions.length <= 5) {
+    const random = questions[Math.floor(Math.random() * questions.length)];
+      if (!shuffledQuestions.includes(random)) {
+        shuffledQuestions.push(random);
       }
-  }
+    }
+}
+
+function NextQuestion(index) {
+  handleQuestions ()
+  const currentQuestion = shuffledQuestions[index];
+    $("#question-number").innerHTML = questionNumber;
+    $("#display-question").innerHTML = currentQuestion.question;
+    $("#option-one-label").innerHTML = currentQuestion.optionA;
+    $("#option-two-label").innerHTML = currentQuestion.optionB;
+    $("#option-three-label").innerHTML = currentQuestion.optionC;
+    $("#option-four-label").innerHTML = currentQuestion.optionD;
+}
   
-  
-  let questionNumber = 1 //holds the current question number
-  let playerScore = 0  //holds the player score
-  let wrongAttempt = 0 //amount of wrong answers picked by player
-  let indexNumber = 0 //will be used in displaying next question
-  
-  // function for displaying next question in the array to dom
-  //also handles displaying players and quiz information to dom
-  function NextQuestion(index) {
-      handleQuestions()
-      const currentQuestion = shuffledQuestions[index]
-      document.getElementById("question-number").innerHTML = questionNumber
-      document.getElementById("player-score").innerHTML = playerScore
-      document.getElementById("display-question").innerHTML = currentQuestion.question;
-      document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
-      document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
-      document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
-      document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
-  
-  
-  }
-  
-  
-  
-  
-  
-  function checkForAnswer() {
-      const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
-      const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
-      const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
-      let correctOption = null
-  
-      options.forEach((option) => {
-          if (option.value === currentQuestionAnswer) {
-              //get's correct's radio input with correct answer
-              correctOption = option.labels[0].id
-          }
-      })
-  
-      //checking to make sure a radio input has been checked or an option being chosen
-      if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
-          document.getElementById('option-modal').style.display = "flex"
+function handleNextQuestion() {
+  checkForAnswer() //check if player picked right or wrong option
+  unCheckRadioButtons()
+  //delays next question displaying for a second just for some effects so questions don't rush in on player
+  setTimeout(() => {
+      if (indexNumber <= 4) {
+//displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
+          NextQuestion(indexNumber)
       }
-  
-      //checking if checked radio button is same as answer
-      options.forEach((option) => {
-          if (option.checked === true && option.value === currentQuestionAnswer) {
-              document.getElementById(correctOption).style.backgroundColor = "light green"
-              playerScore++ //adding to player's score
-              indexNumber++ //adding 1 to index so has to display next question..
-              //set to delay question number till when next question loads
-              setTimeout(() => {
-                  questionNumber++
-              }, 1000)
-          }
-  
-          else if (option.checked && option.value !== currentQuestionAnswer) {
-              const wrongLabelId = option.labels[0].id
-              wrongAttempt++ //adds 1 to wrong attempts 
-              indexNumber++
-              //set to delay question number till when next question loads
-              setTimeout(() => {
-                  questionNumber++
-              }, 1000)
-          }
-      })
-  }
-  
-  
-  
-  //called when the next button is called
-  function handleNextQuestion() {
-      checkForAnswer() //check if player picked right or wrong option
-      unCheckRadioButtons()
-      //delays next question displaying for a second just for some effects so questions don't rush in on player
-      setTimeout(() => {
-          if (indexNumber <= 4) {
-  //displays next question as long as index number isn't greater than 9, remember index number starts from 0, so index 9 is question 10
-              NextQuestion(indexNumber)
-          }
-          else {
-              handleEndGame()//ends game if index number greater than 9 meaning we're already at the 10th question
-          }
-          resetOptionBackground()
-      }, 1000);
-  }
-  
-  //sets options background back to null after display the right/wrong colors
-  function resetOptionBackground() {
-      const options = document.getElementsByName("option");
-      options.forEach((option) => {
-          document.getElementById(option.labels[0].id).style.backgroundColor = ""
-      })
-  }
-  
-  // unchecking all radio buttons for next question(can be done with map or foreach loop also)
-  function unCheckRadioButtons() {
-      const options = document.getElementsByName("option");
-      for (let i = 0; i < options.length; i++) {
-          options[i].checked = false;
+      else {
+          generateRecipe ()//ends game if index number greater than 9 meaning we're already at the 10th question
       }
+      resetOptionBackground()
+  }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+  const options = document.getElementsByName("option");
+  options.forEach((option) => {
+      document.getElementById(option.labels[0].id).style.backgroundColor = ""
+  })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+  const options = document.getElementsByName("option");
+  for (let i = 0; i < options.length; i++) {
+      options[i].checked = false;
   }
-  
-  // function for when all questions being answered
-  function handleEndGame() {
-      let remark = null
-      let remarkColor = null
-  
-   
-      const playerGrade = (playerScore / 5) * 100
-  
-      //data to display to score board
-      document.getElementById('remarks').innerHTML = remark
-      document.getElementById('remarks').style.color = remarkColor
-      document.getElementById('grade-percentage').innerHTML = playerGrade
-      document.getElementById('wrong-answers').innerHTML = wrongAttempt
-      document.getElementById('right-answers').innerHTML = playerScore
-      document.getElementById('score-modal').style.display = "flex"
-  
-  }
-  
-  //closes score modal, resets game and reshuffles questions
-  function closeScoreModal() {
-      questionNumber = 1
-      playerScore = 0
-      wrongAttempt = 0
-      indexNumber = 0
-      shuffledQuestions = []
-      NextQuestion(indexNumber)
-      document.getElementById('score-modal').style.display = "none"
-  }
-  
-  //function to close warning modal
-  function closeOptionModal() {
-      document.getElementById('option-modal').style.display = "none"
-  }
+}
   
 
-///////////////////////////////////////////////Benny////////////////////////////////////////
-fetch("https://trailapi-trailapi.p.rapidapi.com/trails/explore/?lat=%3CREQUIRED%3E&lon=%3CREQUIRED%3E", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "trailapi-trailapi.p.rapidapi.com",
-		"x-rapidapi-key": "71f6b4f193msh561e6e4b6552ca5p13ba72jsne8db3c8baa6c"
+quizTabBtn.click(tabSelectionQuiz);
+formTabBtn.click(tabSelectionForm);
+startBtnEl.click(startQuiz);
+restartBtnEl.click(startQuiz);
+nextBtnEl.click(NextQuestion);
+
+  
+
+
+var ilist = {
+	items: [],
+	dlist: null,
+	iadd: null,
+	inputItem: null,
+	init: function() {
+	  ilist.dlist = document.getElementById("drink-list");
+	  ilist.iadd = document.getElementById("ingredient-add"); 
+	  ilist.inputItem = document.getElementById("drink-item");
+	  ilist.iadd.addEventListener("submit", function(e){
+		  e.preventDefault();
+		  var listitem = document.createElement("li");
+		  var listitemtext = document.createTextNode(ilist.inputItem.value);
+			listitem.appendChild(listitemtext);
+		  document.getElementById('ilist.add').appendChild(listitem);
+		//   console.log(ilist.iadd);
+	  })// where method is, define function method, is add now will become push
+
+	  if (localStorage.items == undefined) {localStorage.items = "[]"; }
+	  ilist.items = JSON.parse(localStorage.items);
+
 	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+  }
 
-
-
-
-// let url = 'trailapi-trailapi.p.rapidapi.com'
-
-
-// fetch(url).then(function (res) { 
-//     console.log(res)
-// });
+  window.addEventListener("DOMContentLoaded", ilist.init);
